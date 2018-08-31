42a3d8e9cfbf4219758d410bd4876a01cfbce907
renanbabinski <renan10babinski@hotmail.com>
2018-08-31 01:28:14 -0300

    finish

--- gulosoadj.c
+++ gulosoadj.c
@@ -2,8 +2,8 @@
 // ******** ALTERAR A LINHA E COLUNA CONFORME O TAMANHO DA MATRIZ *********
 #include <stdio.h>
 #include <stdlib.h>
-#define linha 7
-#define coluna 7
+#define linha 5
+#define coluna 5
 
 int verificaVetor(int*vet,int tamanho,int chave){
 	for(int i=0;i<=tamanho;i++){
@@ -17,15 +17,15 @@ int verificaVetor(int*vet,int tamanho,int chave){
 int main(){
 
 	//Matriz de adjacencia 5x5 da questão A:
-	//int vet [linha][coluna] = {{0,30,-15,0,20},{-30,0,-10,50,0},{15,10,0,5,-10},{0,50,-5,0,-30},{-20,0,10,30,0}};
+	int vet [linha][coluna] = {{0,30,-15,0,20},{-30,0,-10,50,0},{15,10,0,5,-10},{0,50,-5,0,-30},{-20,0,10,30,0}};
 
-	//Matriz de adjacencia 5x8 da questão B:
-	int vet[linha][coluna] = {{0,2,0,6,12,0,0},{-2,0,1,0,0,5,0},{0,-1,0,0,0,0,40},{-6,0,0,0,0,4,0},{-12,0,0,0,0,0,30},
-	{0,-5,0,-4,0,0,8},{0,0,-40,0,-30,-8,0}};
+	//Matriz de adjacencia 7x7 da questão B:
+	//int vet[linha][coluna] = {{0,2,0,6,12,0,0},{-2,0,1,0,0,5,0},{0,-1,0,0,0,0,40},{-6,0,0,0,0,4,0},{-12,0,0,0,0,0,30},
+	//{0,-5,0,-4,0,0,8},{0,0,-40,0,-30,-8,0}};
 
 	int caminho[20];
 
-	int i=0,j=0,menor=0,c=0;
+	int i=0,j=0,menor=0,c=0,custo=0;
 
 	caminho[c++] = j+1;
 	while(1){
@@ -39,7 +39,9 @@ int main(){
 			}
 			j++;
 		}
-		if(menor == 0)
+		if(menor != 0){
+			custo += menor;
+		}else
 			break;
 		i = caminho[c] - 1;
 		j = 0;
@@ -52,6 +54,7 @@ int main(){
 		printf("%d\n",caminho[i]);
 		i++;
 	}
+	printf("\n\nCUSTO: %d\n\n",custo);
 	return 0;
 }
 
--- gulosoinc.c
+++ gulosoinc.c
@@ -2,8 +2,8 @@
 //*******ALTERAR LINHA E COLUNA CONFORME TAMANHO DA MATRIZ*******
 #include <stdio.h>
 #include <stdlib.h>
-#define linha 5
-#define coluna 8
+#define linha 7
+#define coluna 9
 
 int verificaVetor(int*vet,int tamanho,int chave){
 	for(int i=0;i<=tamanho;i++){
@@ -16,55 +16,67 @@ int verificaVetor(int*vet,int tamanho,int chave){
 
 int main(){
 
-	//Matriz de incidencia 7x7 questão A:
-	int vet [linha][coluna] = {{20,0,0,0,0,0,30,-3},{0,0,0,0,50,-10,-30,0},{0,-10,0,5,0,10,0,3},
-	{0,0,-30,-5,50,0,0,0},{-20,10,30,0,0,0,0,0}};
+	//Matriz de incidencia 5x8 questão A:
+	//int vet [linha][coluna] = {{20,0,0,0,0,0,30,-15},{0,0,0,0,50,-10,-30,0},{0,-10,0,5,0,10,0,15},
+	//{0,0,-30,-5,50,0,0,0},{-20,10,30,0,0,0,0,0}};
 
 	//Matriz de incidencia 7x9 questão B:
-	//int vet [linha][coluna] = {{2,0,0,0,6,0,0,12,0},{-2,1,0,5,0,0,0,0,0},{0,-1,40,0,0,0,0,0,0,},
-	//{0,0,0,0,-6,4,0,0,0},{0,0,0,0,0,0,0,-12,30},{0,0,0,-5,0,-4,8,0,0},{0,0,-40,0,0,0,-8,-,-30}};
+	int vet [linha][coluna] = {{2,0,0,0,6,0,0,12,0},{-2,1,0,5,0,0,0,0,0},{0,-1,40,0,0,0,0,0,0,},
+	{0,0,0,0,-6,4,0,0,0},{0,0,0,0,0,0,0,-12,30},{0,0,0,-5,0,-4,8,0,0},{0,0,-40,0,0,0,-8,0,-30}};
 
 	int caminho[20];
 
-	int i=0,j=0,menor=0,c=0,pont=0;
+	int i=0,j=0,menor=0,c=0,pont=0,aux=0,custo=0;
 
 	caminho[c++] = i+1;
 	
 	while(1){
 		while(j < coluna){
 			if(menor == 0 && vet[i][j] > 0){
-				menor = vet[i][j];
-				pont = j;
+				for(aux=0;aux<linha;aux++){
+					if(vet[i][j] == vet[aux][j]*-1)
+						break;
+					if(vet[i][j] == vet[aux][j] && !verificaVetor(&caminho[0],c,aux+1))
+						break;
+				}
+				if(!verificaVetor(&caminho[0],c,aux+1) && aux<linha){
+					menor = vet[i][j];
+					pont = j;
+				}
 			}
 			if(menor > vet[i][j] && vet[i][j] > 0){
-				menor = vet[i][j];
-				pont = j;
+				for(aux=0;aux<linha;aux++){
+					if(vet[i][j] == vet[aux][j]*-1)
+						break;
+					if(vet[i][j] == vet[aux][j] && !verificaVetor(&caminho[0],c,i+1))
+						break;
+				}
+				if(!verificaVetor(&caminho[0],c,aux+1) && aux < linha){
+					menor = vet[i][j];
+					pont = j;
+				}
 			}
 			j++;
 		}
+
+		if(menor != 0){
+			custo += menor;
+		}else{
+			break;
+		}
+
 		for(i=0;i<linha;i++){
 			if(vet[i][pont] == menor*-1)
 				break;
 			if(vet[i][pont] == menor && !verificaVetor(&caminho[0],c,i+1))
 				break;
 		}
-		if(i == 5)
-			i--;
-		if(verificaVetor(&caminho[0],c,i+1))
-			break;
 		caminho[c] = i+1;
 		j=0;
+		pont=0;
 		c++;
 	 	menor=0;
-
-
-
-
-
 	}
-
-
-
 
 	i = 0;
 	printf("\n\nCAMINHO:\n\n");
@@ -72,6 +84,7 @@ int main(){
 		printf("%d\n",caminho[i]);
 		i++;
 	}
+	printf("\n\nCUSTO: %d\n\n",custo);
 	return 0;
 }
 
